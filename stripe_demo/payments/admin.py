2028a80e7a56f14b88b04dd08817883f11ad0c62
destep <tankt34.stepanoff@yandex.ru>
2026-01-10 22:23:41 +0300

    feat: add admin

--- stripe_demo/payments/admin.py
+++ stripe_demo/payments/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Discount, Item, Order, OrderItem, Tax
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "currency")
+    search_fields = ("name",)
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "created_at")
+    inlines = [OrderItemInline]
+    filter_horizontal = ("discounts", "taxes")
+
+
+admin.site.register(Discount)
+admin.site.register(Tax)
